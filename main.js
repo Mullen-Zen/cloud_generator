be215957192e7e0f48e24e4474a2f363b71e0ad2
garrison-pc <mullenzen@gmail.com>
2025-05-04 15:49:14 -0400

    almost donezo

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ import { Cloud } from "./src/cloud";
 import { EffectComposer } from './node_modules/three/examples/jsm/postprocessing/EffectComposer.js';
 import { RenderPass } from './node_modules/three/examples/jsm/postprocessing/RenderPass.js';
 import { UnrealBloomPass } from './node_modules/three/examples/jsm/postprocessing/UnrealBloomPass.js';
+import { update } from "three/examples/jsm/libs/tween.module.js";
 
 let app = {
   el: document.getElementById("app"),
@@ -33,6 +34,21 @@ function addCloudLayer(count, yRange, spread, density = 1.0) {
   }
 }
 
+function updateClock() {
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2, "0");
+  const minutes = now.getMinutes().toString().padStart(2, "0");
+  document.getElementById("clock").textContent = `${hours}:${minutes}`;
+  app.scene.fog = new Fog((now.getHours() <= 5 || now.getHours() >= 19) ? 0x1a2e40 : (now.getHours() <= 8 || now.getHours() >= 17) ? 0xfce9d4 : 0xcce0ff, 10, 50);
+
+  if (now.getHours() <= 5 || now.getHours() >= 19) {
+    document.getElementById("bod").style.background = "linear-gradient(to top, #0a1c2c 0%, #1a2e40 100%)";
+  } else if (now.getHours() <= 8 || now.getHours() >= 17) {
+    document.getElementById("bod").style.background = "linear-gradient(to top, #c2875c 0%, #fce9d4 100%)";
+  } else document.getElementById("bod").style.background = "linear-gradient(to top, #d1eaf5 0%, #ffffff 100%)";
+}
+setInterval(updateClock, 1000);
+
 const init = () => {
   app.renderer = new WebGLRenderer({alpha: true});
   console.log(app.renderer);
@@ -40,6 +56,11 @@ const init = () => {
   app.renderer.shadowMap.enabled = true;
   app.renderer.shadowMap.type = PCFSoftShadowMap; 
   app.el.appendChild (app.renderer.domElement);
+
+  app.scene = new Scene();
+  app.camera =  new PerspectiveCamera( 65, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  app.camera.position.set(-6,-15,0);
+  app.camera.lookAt(-10,0,-10);
 
   app.composer = new EffectComposer(app.renderer);
   app.composer.addPass(new RenderPass(app.scene, app.camera));
@@ -52,12 +73,6 @@ const init = () => {
   );
   app.composer.addPass(bloomPass);
 
-  app.scene = new Scene();
-  app.camera =  new PerspectiveCamera( 65, window.innerWidth / window.innerHeight, 0.1, 1000 );
-  app.camera.position.set(-6,-15,0);
-  app.camera.lookAt(-10,0,-10);
-  app.scene.fog = new Fog(0xcce0ff, 10, 50);
-
   const ambient = new AmbientLight(0xffffff, 0.6);
   app.scene.add(ambient);
 
@@ -68,8 +83,8 @@ const init = () => {
 
   // clouds
   app.clouds = []
-  addCloudLayer(40, [0, 2.5], 2.5); // low
-  addCloudLayer(30, [3.0, 5.0], 2.5);  // mid
+  addCloudLayer(40, [0, 2.5], 3); // low
+  addCloudLayer(30, [3.0, 5.0], 3);  // mid
   addCloudLayer(20, [5.5, 7.5], 4);  // high
   addCloudLayer(5, [2, 4], 2); // add small group at current cam height
 
@@ -108,3 +123,4 @@ const render = () => {
 
 init();
 render();
+updateClock();
